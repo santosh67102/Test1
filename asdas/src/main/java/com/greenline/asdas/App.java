8edaac350335519379817ede2b5c48f1b503205d
A634025 <A634025@V720711.dmn1.fmr.com>
2018-09-07 11:11:21 -0400

    inital commit

---
+++ asdas/src/main/java/com/greenline/asdas/App.java
@@ -0,0 +1,13 @@
+package com.greenline.asdas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
